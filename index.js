ea0ea262ae7328676e5aa0335789d54b4f1e7bcc
zhw2590582 <laozhaochaguan@gmail.com>
2018-04-20 17:55:54 +0800

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = obj => {
+  switch (typeof obj) {
+    case "undefined":
+        return 'undefined';
+    case "object":
+        let type = Object.prototype.toString.call(obj).slice(8, -1).toLowerCase();
+        switch (type) {
+            case "null":
+                return 'null';
+            case "array":
+                return '[' + obj.map(key => objToString(key)).join(', ') + ']';
+            case 'object':
+                return '{ ' + Object.keys(obj).map(key => key + ': ' + objToString(obj[key])).join(', ') + ' }';
+            default:
+                return '[Unknown type: ' + type + ']';
+        }
+    default:
+        return obj.toString();
+  }
+}
